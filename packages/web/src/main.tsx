991508785ad6899c49524318084fae7a5bb6d062
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-02-22 02:40:53 -0800

    genql

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createClient, Provider } from "urql";
+import { useTypedQuery } from "@fangorn/graphql/urql";
+import { useEffect } from "react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+const urql = createClient({
+  url: import.meta.env.VITE_API_URL + "/graphql",
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  // <React.StrictMode>
+  <Provider value={urql}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+  // </React.StrictMode>
+);
+
+function App() {
+  const [helloQueryRes] = useTypedQuery({
+    query: {
+      hello: true,
+    },
+  });
+
+  useEffect(() => {
+    const { fetching, data } = helloQueryRes;
+    if (!fetching && data) {
+      console.log(helloQueryRes);
+    }
+  }, [helloQueryRes]);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>root</div>} />
+        <Route path="*" element={<div>todo</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
